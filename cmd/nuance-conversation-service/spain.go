0619a664c2e0031f9f65ff946626b78122959a8d
Gabe <gabe.susman@gmail.com>
2024-07-17 08:33:57 -0600

    fenced off discord stuff and got it workin

--- cmd/nuance-conversation-service/spain.go
+++ cmd/nuance-conversation-service/spain.go
@@ -1,58 +1,72 @@
 package main
 
 // import (
-// 	"bufio"
 // 	"fmt"
+// 	"log"
 // 	"net"
 // 	"os"
+// 	"os/signal"
 // 	"strings"
+// 	"syscall"
+
+// 	"github.com/bwmarrin/discordgo"
 // )
 
+// var conn net.Conn
+
 // func main() {
-// 	conn, err := connectToServer("localhost:8080")
+// 	// Connect to the TCP server
+// 	var err error
+// 	conn, err = connectToServer("localhost:8080")
 // 	if err != nil {
 // 		fmt.Println("Error connecting:", err)
 // 		os.Exit(1)
 // 	}
 // 	defer conn.Close()
 
-// 	sendAndReceiveMessages(conn)
+// 	// Initialize Discord session
+// 	sess, err := discordgo.New(os.Getenv("DISCORD_BOT_TOKEN"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	// Add message handler
+// 	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+// 		// Ignore messages from the bot itself
+// 		if m.Author.ID == s.State.User.ID {
+// 			return
+// 		}
+
+// 		// Check for the !send command prefix
+// 		if strings.HasPrefix(m.Content, "!send ") {
+// 			msg := strings.TrimPrefix(m.Content, "!send ")
+// 			sendMessageToTCPServer(msg)
+// 			s.ChannelMessageSend(m.ChannelID, "Message sent to TCP server!")
+// 		}
+// 	})
+
+// 	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+// 	err = sess.Open()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer sess.Close()
+
+// 	fmt.Println("The bot is online!")
+
+// 	sc := make(chan os.Signal, 1)
+// 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+// 	<-sc
 // }
 
 // func connectToServer(address string) (net.Conn, error) {
 // 	return net.Dial("tcp", address)
 // }
 
-// func sendAndReceiveMessages(conn net.Conn) {
-// 	go readMessages(conn)
-
-// 	scanner := bufio.NewScanner(os.Stdin)
-// 	fmt.Println("Enter messages to send, type 'exit' to stop:")
-// 	for scanner.Scan() {
-// 		text := scanner.Text()
-// 		if strings.ToLower(text) == "exit" {
-// 			break
-// 		}
-// 		_, err := fmt.Fprintf(conn, text+"\n")
-// 		if err != nil {
-// 			fmt.Println("Error sending message:", err)
-// 			break
-// 		}
-// 	}
-
-// 	if err := scanner.Err(); err != nil {
-// 		fmt.Println("Error reading from stdin:", err)
-// 	}
-// }
-
-// func readMessages(conn net.Conn) {
-// 	reader := bufio.NewReader(conn)
-// 	for {
-// 		message, err := reader.ReadString('\n')
-// 		if err != nil {
-// 			fmt.Println("Error reading from connection:", err)
-// 			return
-// 		}
-// 		fmt.Print("Received from server: ", message)
+// func sendMessageToTCPServer(message string) {
+// 	_, err := fmt.Fprintf(conn, message+"\n")
+// 	if err != nil {
+// 		fmt.Println("Error sending message:", err)
 // 	}
 // }
